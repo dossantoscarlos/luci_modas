e3aac74690fe5dd29cd9511b67ed6e0b556de7e8
Carlos Eduardo <anjoazrael10@gmail.com>
2024-06-07 16:08:45 -0300

    feat(paciente): adicionado rota paciente para listagem

--- paciente/views.py
+++ paciente/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Paciente
 
 # Create your views here.
+def index(request): 
+    
+    paciente_list = list(Paciente.objects.all().values())
+
+    return JsonResponse(paciente_list, safe=False)
