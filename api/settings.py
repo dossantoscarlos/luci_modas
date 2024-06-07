5247ffc1e86c896b5eff78eb0986deb6737fc37c
Carlos Eduardo <anjoazrael10@gmail.com>
2024-06-07 13:22:45 -0300

    renomeando mydatabase

--- api/settings.py
+++ api/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'api.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'mydatabase.sqlite3',
     }
 }
 
