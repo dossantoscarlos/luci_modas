87d03c6595ba1d3205dda06e7dc75f6b3933289d
Carlos  Eduardo dos Santos Silva <anjoazrael10@gmail.com>
2024-07-25 03:41:29 -0300

    feat: personalizando porta

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','django-hello-world-jby4.onrender.com']
 
+PORT =  os.environ.setdefault("PORT", 9040)
 
 # Application definition
 
