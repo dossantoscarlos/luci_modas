26ff2833cf837dc772e36f3f9324652b97c007e2
Carlos  Eduardo dos Santos Silva <anjoazrael10@gmail.com>
2024-08-03 01:13:40 -0300

    criando branch de test

--- clinica/admin.py
+++ clinica/admin.py
@@ -1,88 +1,88 @@
 from django.contrib import admin
-from .models import (
-    Veterinario,
-    Tutor,
-    Pet,
-    Agendamento
-)
+# from .models import (
+#     Veterinario,
+#     Tutor,
+#     Pet,
+#     Agendamento
+# )
 
-class VeterinarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'crm')
-    fieldsets = [
-        ("Dados pessoais", {'fields': ['nome'] }),
-        ("Dados profissionais", { 'fields': ['crm'] })
-    ]
+# class VeterinarioAdmin(admin.ModelAdmin):
+#     list_display = ('nome', 'crm')
+#     fieldsets = [
+#         ("Dados pessoais", {'fields': ['nome'] }),
+#         ("Dados profissionais", { 'fields': ['crm'] })
+#     ]
 
-# Register your models here.
-admin.site.register(Veterinario, VeterinarioAdmin)
+# # Register your models here.
+# admin.site.register(Veterinario, VeterinarioAdmin)
 
-class TutorAdmin(admin.ModelAdmin):
-    list_display = (
-        'nome',
-        'sobrenome',
-        'data_nascimento',
-        'cpf',
-        'sexo',
-        'email',
-        'rg',
-        'tel_contato'
-    )
+# class TutorAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'nome',
+#         'sobrenome',
+#         'data_nascimento',
+#         'cpf',
+#         'sexo',
+#         'email',
+#         'rg',
+#         'tel_contato'
+#     )
 
-    fieldsets = [
-        ("Dados pessoais", {
-            'fields': [
-                'nome',
-                'sobrenome',
-                'data_nascimento',
-                'sexo',
-            ]
-        }),
-        ("Documentos", {
-            'fields': [
-                'cpf', 
-                'rg', 
-            ]
-        }),
-        ("Contato", {
-            'fields': [
-                'email', 
-                'tel_contato', 
-            ]
-        }),
-    ]
+#     fieldsets = [
+#         ("Dados pessoais", {
+#             'fields': [
+#                 'nome',
+#                 'sobrenome',
+#                 'data_nascimento',
+#                 'sexo',
+#             ]
+#         }),
+#         ("Documentos", {
+#             'fields': [
+#                 'cpf', 
+#                 'rg', 
+#             ]
+#         }),
+#         ("Contato", {
+#             'fields': [
+#                 'email', 
+#                 'tel_contato', 
+#             ]
+#         }),
+#     ]
 
-admin.site.register(Tutor, TutorAdmin)
+# admin.site.register(Tutor, TutorAdmin)
 
-class PetAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'data_nascimento', 'porte', 'tutor')
-    fieldsets = [
-        ("Dados pessoais", {
-            'fields': [
-                'nome', 
-                'data_nascimento', 
-                'porte'
-            ]
-        }),
-        ("Responsável", {
-            'fields': [
-                'tutor'
-            ]
-        }),
-    ]
+# class PetAdmin(admin.ModelAdmin):
+#     list_display = ('nome', 'data_nascimento', 'porte', 'tutor')
+#     fieldsets = [
+#         ("Dados pessoais", {
+#             'fields': [
+#                 'nome', 
+#                 'data_nascimento', 
+#                 'porte'
+#             ]
+#         }),
+#         ("Responsável", {
+#             'fields': [
+#                 'tutor'
+#             ]
+#         }),
+#     ]
 
-admin.site.register(Pet, PetAdmin)
+# admin.site.register(Pet, PetAdmin)
 
-class AgendamentoAdmin(admin.ModelAdmin):
-    list_display = ('data', 'tipo_consulta', 'pet', 'veterinario')
-    fieldsets = [
-        ("Dados de agendamento", {
-            'fields': [
-                'data', 
-                'tipo_consulta', 
-                'pet',
-                'veterinario'
-            ]
-        }),
-    ]
+# class AgendamentoAdmin(admin.ModelAdmin):
+#     list_display = ('data', 'tipo_consulta', 'pet', 'veterinario')
+#     fieldsets = [
+#         ("Dados de agendamento", {
+#             'fields': [
+#                 'data', 
+#                 'tipo_consulta', 
+#                 'pet',
+#                 'veterinario'
+#             ]
+#         }),
+#     ]
 
-admin.site.register(Agendamento,AgendamentoAdmin)
+# admin.site.register(Agendamento,AgendamentoAdmin)
