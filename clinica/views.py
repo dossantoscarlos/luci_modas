d989e2fc9994c37d453ed62b02c0bd9dbe010c7f
Carlos  Eduardo dos Santos Silva <anjoazrael10@gmail.com>
2024-08-03 01:14:55 -0300

    removido app clinica

--- clinica/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-from .models import Veterinario
-
-# Create your views here.
-def index(request):
-    paciente_list = list(Veterinario.objects.all().values())
-    return JsonResponse(paciente_list, safe=False)
