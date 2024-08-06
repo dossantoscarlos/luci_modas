938449b2b65646678337c2e00ef518100578086f
Carlos  Eduardo dos Santos Silva <anjoazrael10@gmail.com>
2024-08-06 01:11:19 -0300

    remove(enum):apagado enums referente a categoria

--- loja/choices.py
+++ loja/choices.py
@@ -8,18 +8,3 @@ class ModeloProdutoChoice(models.TextChoices):
     MASCULINO = "M", "MASCULINO"
     FEMININO = "F", "FEMININO"
     UNISEX = "U", "UNISEX"
-
-class CategoriaChoice(models.TextChoices):
-    CALCADO = "C", "Calçado"
-    VESTIARIO= "V", "Vestiário"
-
-class SubCategoriaChoice(models.TextChoices):
-    CAMISA = 0, "Camisa"
-    SAPATO = 1, "Sapato"
-    SAIA = 2, "Saia"
-    VESTIDO = 3, "Vestido"
-    CAMISETA = 4, "Camiseta"
-    CASACO = 5, "Casaco"
-    JAQUETA = 6, "Jaqueta"
-    MOLETON = 7, "Moleton"
-    TENIS = 8, "Tenis"
